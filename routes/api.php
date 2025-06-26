9f07917eadd9b6fdcfdfc5b21b32f9766037eefe
Matheus29lfy <matheus29lfy@gmail.com>
2025-06-26 14:13:32 -0300

    alterado api para controller

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
-
+// routes/api.php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::apiResource('products', ProductController::class);
-
+ Route::apiResource('products', ProductController::class);
+//  Route::get('/products', [ProductController::class, 'index']);
+//  Route::post('/products', [ProductController::class, 'store']);
