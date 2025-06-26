9f19c9518c0e2a02429a58a3f8fd4517a0828cf9
Matheus29lfy <matheus29lfy@gmail.com>
2025-06-26 14:13:07 -0300

    alterado web para get

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
-
+// routes/web.php
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-
-Route::prefix('api')->group(function () {
-    Route::apiResource('products', ProductController::class);
-});
 
 Route::get('/', function () {
     return view('welcome');
