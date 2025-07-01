55e20558f6ac154a1ebb8eb37ef92fb01f4253a8
Matheus29lfy <matheus29lfy@gmail.com>
2025-07-01 14:42:22 -0300

    fix: remocao comentario rotas comentadas

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
  Route::apiResource('products', ProductController::class);
-//  Route::get('/products', [ProductController::class, 'index']);
-//  Route::post('/products', [ProductController::class, 'store']);
